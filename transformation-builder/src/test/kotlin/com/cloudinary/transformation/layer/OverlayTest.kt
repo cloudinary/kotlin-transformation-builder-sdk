d90a7c2f26cc1a1e80b2dfe27c7eb9810d655fc6
mizadi <adi.mizrahi@cloudinary.com>
2022-12-25 11:11:43 +0200

    Fix comment tests on testOverlay

--- transformation-builder/src/test/kotlin/com/cloudinary/transformation/layer/OverlayTest.kt
+++ transformation-builder/src/test/kotlin/com/cloudinary/transformation/layer/OverlayTest.kt
@@ -18,30 +18,30 @@ class OverlayTest {
 
     @Test
     fun testOverlay() {
-//        var overlay = Overlay.source(image("sample")) {
-//            blendMode(multiply())
-//        }
-//
-//        cldAssert("l_sample/e_multiply,fl_layer_apply", overlay)
-//
-//        overlay = Overlay.source(image("sample") {
-//            transformation { resize(scale { width(250) }) }
-//        }) {
-//            position(position)
-//            blendMode(multiply())
-//        }
-//
-//        cldAssert("l_sample/c_scale,w_250/e_multiply,fl_layer_apply,g_cat,x_20", overlay)
-//
-//        overlay = Overlay.source(fetch("https://res.cloudinary.com/demo/image/upload/sample")) {
-//            position(position)
-//        }
-//
-//        cldAssert(
-//            "l_fetch:aHR0cHM6Ly9yZXMuY2xvdWRpbmFyeS5jb20vZGVtby9pbWFnZS91cGxvYWQvc2FtcGxl/fl_layer_apply,g_cat,x_20",
-//            overlay
-//        )
-        var overlay = Overlay.text {
+        var overlay = Overlay.source(image("sample")) {
+            blendMode(multiply())
+        }
+
+        cldAssert("l_sample/e_multiply,fl_layer_apply", overlay)
+
+        overlay = Overlay.source(image("sample") {
+            transformation { resize(scale { width(250) }) }
+        }) {
+            position(position)
+            blendMode(multiply())
+        }
+
+        cldAssert("l_sample/c_scale,w_250/e_multiply,fl_layer_apply,g_cat,x_20", overlay)
+
+        overlay = Overlay.source(fetch("https://res.cloudinary.com/demo/image/upload/sample")) {
+            position(position)
+        }
+
+        cldAssert(
+            "l_fetch:aHR0cHM6Ly9yZXMuY2xvdWRpbmFyeS5jb20vZGVtby9pbWFnZS91cGxvYWQvc2FtcGxl/fl_layer_apply,g_cat,x_20",
+            overlay
+        )
+        overlay = Overlay.text {
             source("hello world!") {
                 style("Arial", 17)
                 textColor(Color.RED)
